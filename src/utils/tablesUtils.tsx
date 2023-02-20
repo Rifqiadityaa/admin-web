7ca8d928d5fcfb24d939266dc8209a897aa4e5f2
LAPTOP-POKTB6DS\Rifqi <rifqiadityaa24@gmail.com>
2023-02-21 01:04:17 +0700

    split functions into utils and put them in different file

---
+++ src/utils/tablesUtils.tsx
@@ -0,0 +1,14 @@
+import {
+  GridToolbarContainer,
+  GridToolbarFilterButton,
+  GridToolbarExport,
+} from "@mui/x-data-grid";
+
+export function CustomToolbar() {
+  return (
+    <GridToolbarContainer>
+      <GridToolbarFilterButton />
+      <GridToolbarExport />
+    </GridToolbarContainer>
+  );
+}
